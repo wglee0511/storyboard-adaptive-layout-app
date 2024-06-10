d7e4c9459d35c5c7a29dd29738511939fec245ac
wglee0511 <ranazang@naver.com>
2024-06-10 21:06:49 +0900

    Initial Commit

---
+++ StortboardAdaptiveLayoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StortboardAdaptiveLayoutApp
+//
+//  Created by racoon on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
